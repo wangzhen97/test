5561b0673cbabe1668e478d8ed225aea71456281
wangzhen97 <1173375190@qq.com>
2020-02-25 11:53:30 +0800

    hello

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+class HelloWorld{
+public static void main(String[]args){
+	System.out.printIn("1);
+System.out.printIn("hellojing");
+System.out.printIn("2");
+System.out.printIn("hellojing");
+
+}
+
+}
